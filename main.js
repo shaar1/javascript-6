1f8ff7a1cd493e1fe8f22baad4f32249c8e831f9
Abdallah <abdallahchaar90@gmail.comh>
2021-01-01 16:22:30 +0100

    träning

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var mainPrice = 500;
+var mainPrice = 500,
 
-    mySmallDiscount = 50;
+    mySmallDiscount = 50,
 
-    myMediumDiscount = 100;
+    myMediumDiscount = 100,
 
     myBigDiscount = 250;
 
@@ -13,3 +13,20 @@ document.getElementById("product1").innerHTML = mainPrice - mySmallDiscount; // 
 document.getElementById("product2").innerHTML = mainPrice - myMediumDiscount; // 500 - 100 = 400 
 
 document.getElementById("product3").innerHTML = mainPrice - myBigDiscount; // 500 - 250 = 250 
+______________________________________________
+
+var mainPrice = 400,
+
+    mySmallDiscount = 50,
+
+    myMediumDiscount = 100,
+
+    myBigDiscount = 250;
+
+document.getElementById("price").innerHTML = mainPrice;
+
+document.getElementById("product1").innerHTML = mainPrice - mySmallDiscount; // 400 - 50 = 350 
+
+document.getElementById("product2").innerHTML = mainPrice - myMediumDiscount; // 400 - 100 = 300 
+
+document.getElementById("product3").innerHTML = mainPrice - myBigDiscount; // 400 - 250 = 150 
